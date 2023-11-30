8d27443400e01bb47ec6850ffa7538b3382adc43
Filip824 <filip.petkovski.work@gmail.com>
2023-11-30 17:04:05 +0100

    Commit 2

---
+++ Homework 2/wine_model/src/main/java/com/example/wine_model/WineModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.wine_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineModelApplication.class, args);
+    }
+
+}
---
+++ Homework 2/wine_model/src/test/java/com/example/wine_model/WineModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wine_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
