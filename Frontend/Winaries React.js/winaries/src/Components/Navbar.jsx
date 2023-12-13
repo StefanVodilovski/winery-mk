8357400c9512b633de3ebbb012998729134dcfcc
NikolaAlchev <nikolalcev2002@gmail.com>
2023-12-13 20:53:10 +0100

    Home page

--- Frontend/Winaries React.js/winaries/src/Components/Navbar.jsx
+++ Frontend/Winaries React.js/winaries/src/Components/Navbar.jsx
@@ -1,26 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, NavLink } from "react-router-dom"
-import logoImage from '../images/logoImage.png';
+import logoImage from '../images/logoWhite.png';
 import BagImage from '../images/BagImage.png';
 import "./Navbar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBagShopping, faBars} from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = () => {
+
+    const [scrollVh, setScrollVh] = useState(0);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const scrollPositionPixels = window.scrollY;
+        const viewportHeight = window.innerHeight;
+        const scrollPositionVh = (scrollPositionPixels / viewportHeight) * 100;
+        setScrollVh(scrollPositionVh);
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+
+    const navClass = scrollVh > 30 ? 'scrolled' : '';
+
     const [menuOpen, setMenuOpen] = useState(false)
     return (
-        <nav>
+        <nav className={navClass}>
             <Link to="/" className='title'>
-                <img src={logoImage} />
+                <div className='logo'></div>
             </Link>
+            
             <div className='menu' onClick={() => {
                 setMenuOpen(!menuOpen)
             }}>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+                <FontAwesomeIcon icon={faBars} size="2xl"/>
             </div>
             <ul className={menuOpen ? "open" : ""} >
                 <li> <NavLink to="/"> Home </NavLink> </li>
@@ -29,10 +46,13 @@ export const Navbar = () => {
                 <li> <NavLink to="/events"> Events </NavLink> </li>
                 <li> <NavLink to="/map"> Map </NavLink> </li>
             </ul>
-            <ul className={menuOpen ? "open" : ""} name="right_end">
-                <li><NavLink to="/cart"><img src={BagImage} alt='bagImage' /></NavLink></li>
-                <li><NavLink to="/register">My profile</NavLink></li>
-            </ul>
+            <div className='right-end'>
+                <ul className={menuOpen ? "open" : ""} name="right_end">
+                    <li><NavLink to="/cart"><FontAwesomeIcon icon={faBagShopping} className='shopping-cart-icon'/></NavLink></li>
+                    <li><NavLink to="/register">Login</NavLink></li>
+                </ul>
+            </div>
+            
         </nav>
     )
 }
--- Frontend/Winaries React.js/winaries/src/Components/Pages/Home.jsx
+++ Frontend/Winaries React.js/winaries/src/Components/Pages/Home.jsx
@@ -4,13 +4,18 @@ import "./css/Home.css"
 import { Carousel } from '../Carousel';
 
 
-
-
 export const Home = () => {
     return (
-        <div>
-            <div>
-                <img src={backgoundImg} alt="" className='bckImg' />
+        <div className='home'>
+            <div className="background-container">
+                <div className='title-container'>
+                    <h1 className='welcome-title'>
+                        WELCOME
+                    </h1>
+                    <h2 className='slogan'>
+                        Sip elegance, taste excellence
+                    </h2>
+                </div>
             </div>
             <Carousel />
         </div>
