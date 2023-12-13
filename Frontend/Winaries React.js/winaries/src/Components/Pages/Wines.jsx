d0ee0f5080f1d7defb2523947518da24f0405821
stefan <vodilovski@gmail.com>
2023-12-13 21:04:34 +0100

    wines small changes

--- Frontend/Winaries React.js/winaries/src/Components/Pages/Wines.jsx
+++ Frontend/Winaries React.js/winaries/src/Components/Pages/Wines.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import "./css/Wines.css"
+
 export const Wines = () => {
     return (
-        <div>
-            Wines
+        <div >
+            <div className='search-bar-container'>
+                <div>searchBar
+                </div>
+                <div>
+                    SearchResults
+                </div>
+            </div>
         </div>
     )
 }
