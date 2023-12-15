cea5b571c805e04875dc3e047b20a06797c830de
stefan <vodilovski@gmail.com>
2023-12-15 22:48:27 +0100

    wines changes

--- Frontend/Winaries React.js/winaries/src/Components/Searchbar.jsx
+++ Frontend/Winaries React.js/winaries/src/Components/Searchbar.jsx
@@ -6,11 +6,18 @@ export const Searchbar = ({ setResults }) => {
     const [input, setInput] = useState("")
 
     const fetchData = (value) => {
-        fetch('https://jsonplaceholder.typicode.com/users')
+        // ova url e /wines/filter 
+        // searchQuery=value.
+        // priceFilter=
+        // region=
+        // winery=
+        // literage= 
+        let url = 'https://jsonplaceholder.typicode.com'
+        fetch(url)
             .then((response) => response.json())
             .then((json) => {
-                const results = json.filter((users) => {
-                    return (value && users && users.name && users.name.toLowerCase().includes(value))
+                const results = json.filter((item) => {
+                    return (value && item && item.name && item.name.toLowerCase().includes(value))
                 });
                 setResults(results)
             });
@@ -26,6 +33,7 @@ export const Searchbar = ({ setResults }) => {
             <FaSearch id='search-icon' />
             <input placeholder="searc wines"
                 value={input}
+                name='search'
                 onChange={(e) => handleChange(e.target.value)}
             />
         </div>
