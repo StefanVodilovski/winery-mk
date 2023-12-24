71a5844b02f7d3f4d04e4fcf2bd78715ae76fb89
stefan <vodilovski@gmail.com>
2023-12-24 23:08:31 +0100

    changed file structure

--- Homework 2/wine_model/src/main/java/com/example/wine_model/repository/WineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.wine_model.repository;
-
-import com.example.wine_model.model.Wine;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WineRepository extends JpaRepository<Wine,Long> {
-}
--- Homework 2/wine_model/src/main/java/com/example/wine_model/service/WineService.java
+++
@@ -1,9 +0,0 @@
-package com.example.wine_model.service;
-
-import com.example.wine_model.model.Wine;
-
-import java.util.List;
-
-public interface WineService {
-    List<Wine> getAll();
-}
