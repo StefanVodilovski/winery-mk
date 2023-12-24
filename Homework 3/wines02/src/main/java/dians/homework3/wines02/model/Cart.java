b402896912deebd0ea1b69b219349643f24cecde
filipetkovski <filip.petkovski.work@gmail.com>
2023-12-24 15:19:04 +0100

    Commit 34

--- Homework 3/wines02/src/main/java/dians/homework3/wines02/model/Cart.java
+++ Homework 3/wines02/src/main/java/dians/homework3/wines02/model/Cart.java
@@ -30,7 +30,7 @@ public class Cart {
     @JoinColumn(name = "created_by")
     private UserEntity createdBy = null;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     @Fetch(FetchMode.SELECT)
     private List<AddWines> cartWines = new ArrayList<>();
 
--- Homework 3/wines02/src/main/java/dians/homework3/wines02/repository/CartRepository.java
+++ Homework 3/wines02/src/main/java/dians/homework3/wines02/repository/CartRepository.java
@@ -1,8 +1,12 @@
 package dians.homework3.wines02.repository;
 
 import dians.homework3.wines02.model.Cart;
+import dians.homework3.wines02.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
